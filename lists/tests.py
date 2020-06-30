484d70ebdbad3bc0922f46092df7af34954a5952
Marcela Vieira <marcelafvieira@gmail.com>
2020-06-29 22:18:04 -0300

    Front page HTML generated from template; Black

--- lists/tests.py
+++ lists/tests.py
@@ -5,7 +5,6 @@ from lists.views import home_page
 
 
 class HomePageTest(TestCase):
-
     def test_home_page_returns_correct_html(self):
-        response = self.client.get('/')
-        self.assertTemplateUsed(response, 'wrong.html')
+        response = self.client.get("/")
+        self.assertTemplateUsed(response, "wrong.html")
